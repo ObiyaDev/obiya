b8b565358aec1184758e5c426a92e498a1c9f8b3
Mike Piccolo <mfpiccolo@gmail.com>
2025-01-15 12:12:41 -0600

    add base styling for nodes

--- playground/steps/parallelMergeState/joinComplete.step.tsx
+++ playground/steps/parallelMergeState/joinComplete.step.tsx
@@ -5,7 +5,7 @@ export default ({ data }: BaseNodeProps) => {
   return (
     <BaseNode
       data={data}
-      variant="ghost"
+      variant="noop"
       excludePubsub
       className="py-4 px-8 flex flex-col max-w-[300px] bg-black border-white rounded-full text-white border border-solid text-center text-sm"
     />
