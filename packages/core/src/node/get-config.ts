9d7f34470e81afb2e65514a28207daf906fbaaf3
Rodrigo Morales <rodrigo@rodrigomorales.co>
2025-01-19 23:21:46 -0800

    fix: loading step files

--- packages/core/src/node/get-config.ts
+++ packages/core/src/node/get-config.ts
@@ -1,11 +1,27 @@
 import { StepConfig } from "../types";
-import { getModuleExport } from "./get-module-export";
+import path from 'path';
+import * as tsconfigPaths from 'tsconfig-paths';
+
+const tsConfigPath = path.resolve(process.cwd(), 'tsconfig.json');
+const result = tsconfigPaths.loadConfig(tsConfigPath);
+
+if (result.resultType !== 'success') {
+  throw Error('Failed to load tsconfig.json');
+}
+
+const {absoluteBaseUrl, paths} = result;
+
+require('ts-node').register({
+  transpileOnly: true,
+  compilerOptions: { module: 'commonjs', baseUrl: absoluteBaseUrl, paths },
+})
 
 export const getNodeFileConfig = async (filePath: string): Promise<StepConfig> => {
   try {
-    const config = await getModuleExport(filePath, 'config');
+    const resolvedFilePath = require.resolve(filePath);
+    const module = require(resolvedFilePath);
 
-    return config;
+    return module.config;
   } catch (error) {
     console.error(`Failed to extract config from ${filePath}:`, error);
 
--- packages/core/src/node/load-node-file-exports.ts
+++ packages/core/src/node/load-node-file-exports.ts
@@ -1,6 +1,7 @@
 import path from "path";
 import { build } from "esbuild";
 import vm from "vm";
+import esbuildPluginTsc from 'esbuild-plugin-tsc';
 
 export const loadNodeFileExports = async <T>(filePath: string): Promise<T> => {
   try {
@@ -13,8 +14,11 @@ export const loadNodeFileExports = async <T>(filePath: string): Promise<T> => {
       bundle: true,
       platform: "node",
       format: "cjs",
+      target: ["es2020"],
       write: false, // Keep output in memory
       external: ["@motia/core"], // Add external dependencies to exclude if needed
+      loader: { '.ts': 'ts' }, // Add this line to handle TypeScript files
+      plugins: [esbuildPluginTsc()],
     });
 
     const bundledCode = result.outputFiles[0].text;
@@ -22,10 +26,19 @@ export const loadNodeFileExports = async <T>(filePath: string): Promise<T> => {
     // Step 3: Create a sandboxed environment
     const sandbox: Record<string, any> = {};
     const moduleExports: Record<string, any> = {};
+    sandbox.process = process;
+    sandbox.global = global;
+    sandbox.require = require;
+  
+    for (const moduleName of require('module').builtinModules) {
+      sandbox[moduleName] = require(moduleName);
+    }
+
     sandbox.exports = moduleExports;
     sandbox.module = { exports: moduleExports };
 
     // Step 4: Execute the bundled code in the sandbox
+    
     const script = new vm.Script(bundledCode);
     const context = vm.createContext(sandbox);
     script.runInContext(context);
