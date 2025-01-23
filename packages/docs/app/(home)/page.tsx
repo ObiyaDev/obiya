f1e20b945cdd5c8ad0a2a939b0d537658c39c2a1
Rodrigo Morales <rodrigo@rodrigomorales.co>
2025-01-22 23:38:33 -0800

    chore: update home page

--- packages/docs/app/(home)/page.tsx
+++ packages/docs/app/(home)/page.tsx
@@ -3,16 +3,15 @@ import Link from 'next/link';
 export default function HomePage() {
   return (
     <main className="flex flex-1 flex-col justify-center text-center">
-      <h1 className="mb-4 text-2xl font-bold">Hello World</h1>
+      <h1 className="mb-4 text-2xl font-bold">Motia</h1>
       <p className="text-fd-muted-foreground">
-        You can open{' '}
+        Let's build some cool stuff together! <br />
         <Link
           href="/docs"
           className="text-fd-foreground font-semibold underline"
         >
           /docs
         </Link>{' '}
-        and see the documentation.
       </p>
     </main>
   );
