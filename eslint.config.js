aac8edc348ff7a355d5b7c29917941fd796c6dbc
Sergio Marcelino <sergio@filho.org>
2025-01-25 07:57:13 -0300

    feat: introducing linters to pipeline

---
+++ eslint.config.js
@@ -0,0 +1,33 @@
+const globals = require('globals')
+const pluginJs = require('@eslint/js')
+const pluginTs = require('@typescript-eslint/eslint-plugin')
+const parserTs = require('@typescript-eslint/parser')
+const pluginJest = require('eslint-plugin-jest')
+
+/** @type {import('eslint').Linter.Config[]} */
+module.exports = [
+  {
+    files: ['index.ts', 'src/**/*.ts', 'test/**/*.ts', 'steps/**/*.ts', 'integration-tests/**/*.ts'],
+    languageOptions: {
+      parser: parserTs,
+      parserOptions: {
+        project: './tsconfig.json',
+      },
+      globals: {
+        ...globals.node,
+        ...pluginJest.environments.globals.globals,
+        NodeJS: 'readonly',
+        Buffer: 'readonly',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': pluginTs,
+      jest: pluginJest,
+    },
+    rules: {
+      ...pluginJs.configs.recommended.rules,
+      ...pluginTs.configs.recommended.rules,
+      ...pluginJest.configs.recommended.rules,
+    },
+  },
+]
