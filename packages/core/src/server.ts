14758fb9e079a5268f755a0cd7c17af247c7f0d6
Sergio Marcelino <sergio@filho.org>
2025-06-06 16:23:08 -0300

    fix: fixing a few issues in streams and python runtime

--- packages/core/src/server.ts
+++ packages/core/src/server.ts
@@ -25,7 +25,7 @@ import { flowsConfigEndpoint } from './flows-config-endpoint'
 import { apiEndpoints } from './streams/api-endpoints'
 import { createSocketServer } from './socket-server'
 import { Log, LogsStream } from './streams/logs-stream'
-import { BaseStreamItem, IStateStream, StateStreamEventChannel, StateStreamEvent } from './types-stream'
+import { BaseStreamItem, MotiaStream, StateStreamEventChannel, StateStreamEvent } from './types-stream'
 import { analyticsEndpoint } from './analytics-endpoint'
 import { trackEvent } from './analytics/utils'
 
@@ -77,58 +77,61 @@ export const createServer = async (
     },
   })
 
-  lockedData.applyStreamWrapper(state, (streamName, stream) => {
-    return (): IStateStream<BaseStreamItem> => {
-      const suuper = stream()
+  lockedData.applyStreamWrapper((streamName, stream) => {
+    return (): MotiaStream<BaseStreamItem> => {
+      const main = stream() as MotiaStream<BaseStreamItem>
       // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      const wrapObject = (groupId: string, id: string, object: any) => ({
-        ...object,
-        __motia: { type: 'state-stream', streamName, groupId, id },
-      })
+      const wrapObject = (groupId: string, id: string, object: any) => {
+        if (!object) {
+          return null
+        }
 
-      const wrapper: IStateStream<BaseStreamItem> = {
-        ...suuper,
-
-        async send<T>(channel: StateStreamEventChannel, event: StateStreamEvent<T>) {
-          pushEvent({ streamName, ...channel, event: { type: 'event', event } })
-        },
-
-        async get(groupId: string, id: string) {
-          const result = await suuper.get.apply(wrapper, [groupId, id])
-          return wrapObject(groupId, id, result)
-        },
-
-        async set(groupId: string, id: string, data: BaseStreamItem) {
-          if (!data) {
-            return null
-          }
-
-          const exists = await suuper.get(groupId, id)
-          const updated = await suuper.set.apply(wrapper, [groupId, id, data])
-          const result = updated ?? data
-          const wrappedResult = wrapObject(groupId, id, result)
-
-          const type = exists ? 'update' : 'create'
-          pushEvent({ streamName, groupId, id, event: { type, data: result } })
-
-          return wrappedResult
-        },
-
-        async delete(groupId: string, id: string) {
-          const result = await suuper.delete.apply(wrapper, [groupId, id])
-
-          pushEvent({ streamName, groupId, id, event: { type: 'delete', data: result } })
-
-          return wrapObject(groupId, id, result)
-        },
-
-        async getGroup(groupId: string) {
-          const list = await suuper.getGroup.apply(wrapper, [groupId])
-          return list.map((object: BaseStreamItem) => wrapObject(groupId, object.id, object))
-        },
+        return {
+          ...object,
+          __motia: { type: 'state-stream', streamName, groupId, id },
+        }
       }
 
-      return wrapper
+      const mainGetGroup = main.getGroup
+      const mainGet = main.get
+      const mainSet = main.set
+      const mainDelete = main.delete
+
+      main.getGroup = async (groupId: string) => {
+        const result = await mainGetGroup.apply(main, [groupId])
+        return result.map((object: BaseStreamItem) => wrapObject(groupId, object.id, object))
+      }
+
+      main.get = async (groupId: string, id: string) => {
+        const result = await mainGet.apply(main, [groupId, id])
+        return wrapObject(groupId, id, result)
+      }
+
+      main.set = async (groupId: string, id: string, data: BaseStreamItem) => {
+        if (!data) {
+          return null
+        }
+
+        const exists = await main.get(groupId, id)
+        const updated = await mainSet.apply(main, [groupId, id, data])
+        const result = updated ?? data
+        const wrappedResult = wrapObject(groupId, id, result)
+
+        const type = exists ? 'update' : 'create'
+        pushEvent({ streamName, groupId, id, event: { type, data: result } })
+
+        return wrappedResult
+      }
+
+      main.delete = async (groupId: string, id: string) => {
+        const result = await mainDelete.apply(main, [groupId, id])
+
+        pushEvent({ streamName, groupId, id, event: { type: 'delete', data: result } })
+
+        return wrapObject(groupId, id, result)
+      }
+
+      return main
     }
   })
 
@@ -176,6 +179,7 @@ export const createServer = async (
         })
 
         if (!result) {
+          console.log('no result')
           res.status(500).json({ error: 'Internal server error' })
           return
         }
