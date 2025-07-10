14a0ef4d5e9a9b0973a515f78667c7908a5b3859
Anderson Leal <andersonofl@gmail.com>
2025-07-10 19:50:52 -0300

    refactor: simplify StateDetail component by removing unused state and restructuring sidebar tabs

--- packages/workbench/src/components/states/state-detail.tsx
+++ packages/workbench/src/components/states/state-detail.tsx
@@ -1,6 +1,6 @@
 import { Sidebar } from '@/components/sidebar/sidebar'
-import { Braces, X } from 'lucide-react'
-import React, { useState } from 'react'
+import { X } from 'lucide-react'
+import React from 'react'
 import JsonView from 'react18-json-view'
 import { StateItem } from './hooks/states-hooks'
 import { StateValue } from './state-value'
@@ -11,26 +11,24 @@ type Props = {
 }
 
 export const StateDetail: React.FC<Props> = ({ state, onClose }) => {
-  const [isCodeEnabled, setIsCodeEnabled] = useState(false)
-
   return (
     <Sidebar
       onClose={onClose}
       title="State details"
       subtitle={`${state.groupId} ${state.key}`}
-      actions={[
+      tabs={[
         {
-          active: isCodeEnabled,
-          icon: <Braces />,
-          onClick: () => setIsCodeEnabled(!isCodeEnabled),
-          label: 'Code',
+          label: 'Details',
+          content: <StateValue value={state.value} isRoot />,
         },
+        {
+          label: 'JSON',
+          content: <JsonView src={state.value} />,
+        },
+      ]}
+      actions={[
         { icon: <X />, onClick: onClose, label: 'Close' },
       ]}
-    >
-      <div className="flex flex-col gap-2">
-        {isCodeEnabled ? <JsonView src={state.value} /> : <StateValue value={state.value} isRoot />}
-      </div>
-    </Sidebar>
+    />
   )
 }
