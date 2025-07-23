96c20a1c690e7b53c4a5fb7f3fed20021b418779
Anderson Leal <andersonofl@gmail.com>
2025-07-23 17:46:14 -0300

    feat: show the step source code in the step detail sidepanel

--- packages/core/src/server.ts
+++ packages/core/src/server.ts
@@ -32,6 +32,7 @@ import {
 import { BaseStreamItem, MotiaStream, StateStreamEvent, StateStreamEventChannel } from './types-stream'
 import { globalLogger } from './logger'
 import { Printer } from './printer'
+import { stepEndpoint } from './step-endpoint'
 
 export type MotiaServer = {
   app: Express
@@ -259,6 +260,7 @@ export const createServer = (
   flowsEndpoint(lockedData)
   flowsConfigEndpoint(app, process.cwd(), lockedData)
   analyticsEndpoint(app, process.cwd())
+  stepEndpoint(app, lockedData)
 
   server.on('error', (error) => {
     console.error('Server error:', error)
