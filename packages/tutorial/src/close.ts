4718ad60d205b93d1aa50087505cbe8202846515
Rodrigo Morales <rodrigo@rodrigomorales.co>
2025-08-06 19:47:20 -0700

    fix: remove set local storage skip on close, needs to be on done

--- packages/tutorial/src/close.ts
+++ packages/tutorial/src/close.ts
@@ -3,10 +3,5 @@ import driver from './driver'
 export const closeTutorial = (skipDestroyCall?: boolean) => {
   console.log('Closing tutorial', { skipDestroyCall })
   // TODO: add analytics
-
-  if (!skipDestroyCall) {
-    driver().destroy()
-  }
-
-  window.localStorage.setItem('motia-tutorial-skipped', 'true')
+  driver().destroy()
 }
