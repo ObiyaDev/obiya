c89545795d2800e340be6e46bf1695280ab61c84
Rohit Ghumare <ghumare64@gmail.com>
2025-08-08 22:11:07 +0100

    update readme and footer

--- packages/docs/components/Footer.tsx
+++ packages/docs/components/Footer.tsx
@@ -60,6 +60,31 @@ const SocialLinks: React.FC = () => {
           {twitterIcon} <p>Follow us on Twitter</p>
         </Link>
       </div>
+      <div className="mt-4">
+        <a
+          href="https://vercel.com/oss?utm_source=motia&utm_campaign=oss"
+          target="_blank"
+          rel="noreferrer"
+          className="flex items-center gap-2 text-sm text-gray-400 mt-2"
+        >
+          <svg
+            aria-label="Vercel logomark"
+            height="15"
+            role="img"
+            viewBox="0 0 74 64"
+            className="fill-current"
+          >
+            <path
+              d="M37.5896 0.25L74.5396 64.25H0.639648L37.5896 0.25Z"
+              fill="white"
+            ></path>
+          </svg>
+          <span className="flex flex-col">
+            <span>Vercel Inc. // 2025</span>
+            <span>Open Source Software Program</span>
+          </span>
+        </a>
+      </div>
     </div>
   )
 }
