bc2dc2f2321b1362c12b96bacf04b026bcf53dec
Rodrigo Morales <rodrigo@rodrigomorales.co>
2025-08-08 22:33:45 -0700

    fix: linting

--- packages/workbench/src/components/ui/tutorial-button.tsx
+++ packages/workbench/src/components/ui/tutorial-button.tsx
@@ -3,10 +3,10 @@ import { Button } from '@motiadev/ui'
 import { Book } from 'lucide-react'
 import { useStreamItem } from '@motiadev/stream-client-react'
 import { FlowConfigResponse } from '@/types/flow'
-import { useEffect } from 'react'
+import { FC, useCallback, useEffect } from 'react'
 import { useFlowStore } from '@/stores/use-flow-store'
 
-export const TutorialButton: React.FC = () => {
+export const TutorialButton: FC = () => {
   const selectFlowId = useFlowStore((state) => state.selectFlowId)
   const { data: flowConfig } = useStreamItem<FlowConfigResponse>({
     streamName: '__motia.flowsConfig',
@@ -14,28 +14,29 @@ export const TutorialButton: React.FC = () => {
     id: 'basic-tutorial',
   })
 
-  const kickStartTutorial = (resetState = false) => {
-    const tutorialStepIndex = new URLSearchParams(window.location.search).get('tutorialStepIndex')
-    const config: TutorialConfig = {
-      resetSkipState: resetState,
-    }
-    if (tutorialStepIndex && !resetState) {
-      config.initialStepIndex = Number(tutorialStepIndex)
-    }
-    selectFlowId('basic-tutorial')
-    setTimeout(() => {
+  const kickStartTutorial = useCallback(
+    (resetState = false) => {
+      const tutorialStepIndex = new URLSearchParams(window.location.search).get('tutorialStepIndex')
+      const config: TutorialConfig = {
+        resetSkipState: resetState,
+      }
+      if (tutorialStepIndex && !resetState) {
+        config.initialStepIndex = Number(tutorialStepIndex)
+      }
+      selectFlowId('basic-tutorial')
       MotiaTutorial.start(config)
-    }, 300)
 
-    if (resetState) {
-      const url = new URL(window.location.href)
-      url.searchParams.delete('tutorialStepIndex')
-      window.history.replaceState(null, '', url)
-    }
-  }
+      if (resetState) {
+        const url = new URL(window.location.href)
+        url.searchParams.delete('tutorialStepIndex')
+        window.history.replaceState(null, '', url)
+      }
+    },
+    [selectFlowId],
+  )
 
   useEffect(() => {
-    if (import.meta.env.MOTIA_TUTORIAL_DISABLED || !flowConfig) {
+    if (import.meta.env.VITE_MOTIA_TUTORIAL_DISABLED || !flowConfig) {
       console.log('Tutorial disabled or flow not found')
       return
     }
@@ -43,9 +44,9 @@ export const TutorialButton: React.FC = () => {
     kickStartTutorial()
 
     return () => MotiaTutorial.close()
-  }, [import.meta.env.MOTIA_TUTORIAL_DISABLED, flowConfig])
+  }, [flowConfig, kickStartTutorial])
 
-  if (import.meta.env.MOTIA_TUTORIAL_DISABLED) {
+  if (import.meta.env.VITE_MOTIA_TUTORIAL_DISABLED) {
     return null
   }
 
