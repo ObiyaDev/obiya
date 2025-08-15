000HAZEM7/motia

feat: deploy simplification

packages/snap/src/cloud/cli/deploy.ts
@@ -1,61 +1,55 @@
 import colors from 'colors'
-import path from 'path'
-import { build } from '../build'
+import { buildValidation } from '../build/build-validation'
 import { cloudCli } from '../cli'
 import { handler } from '../config-utils'
-import { VersionManager } from '../deploy/deploy'
-import { validateStepsConfig } from '../deploy/utils/validation'
+import { build } from '../new-deployment/build'
+import { cloudApi } from '../new-deployment/cloud-api'
+import { deploy } from '../new-deployment/deploy'
+import { CliListener } from '../new-deployment/listeners/cli-listener'
+import { uploadArtifacts } from '../new-deployment/upload-artifacts'
+import { loadEnvData } from '../new-deployment/utils/load-env-data'
 
 cloudCli
   .command('deploy')
   .description('Deploy a new version to Motia Cloud')
   .requiredOption('-k, --api-key <key>', 'The API key for authentication', process.env.MOTIA_API_KEY)
   .requiredOption('-v, --version-name <version>', 'The version to deploy')
-  .option('-p, --project-id <id>', 'Override the selected project')
-  .option('-s, --environment-id <id>', 'Override the selected environment')
+  .option('-p, --project-id <id>', 'Project ID')
+  .option('-s, --environment-id <id>', 'Environment ID')
   .option('-e, --env-file <path>', 'Path to environment file')
   .action(
     handler(async (arg, context) => {
-      const versionManager = new VersionManager()
-      const builder = await build(context)
-      const { errors, warnings } = validateStepsConfig(builder)
+      const listener = new CliListener(context)
+      const builder = await build(listener)
+      const isValid = buildValidation(builder, listener)
 
-      if (warnings.length > 0) {
-        warnings.map((warning, index) => {
-          context.log(`build-warnings-${index}`, (message) => {
-            message.tag('warning').append(warning.message)
-          })
-        })
-      }
-
-      if (errors.length > 0) {
-        context.log('build-failed', (message) => {
-          message.box(['Unable to deploy to Motia Cloud, please fix the following errors'], 'red')
-        })
-        console.log('')
-
-        const errorTag = colors.red('✗ [ERROR]')
-
-        errors.map((error) => {
-          const relativePath = path.relative(builder.projectDir, error.step.filePath)
-          const filePath = colors.gray(`[${relativePath}]`)
-          console.log(`${errorTag} ${filePath} ${error.message}`)
-        })
-
-        console.log(colors.gray('\n--------------------------------\n'))
-        context.log('build-failed-end', (message) => {
-          message.tag('failed').append('Deployment canceled', 'red')
-        })
+      if (!isValid) {
         process.exit(1)
       }
 
       console.log(colors.green('✓ [SUCCESS]'), 'Build completed')
 
-      await versionManager.deploy(context, process.cwd(), arg.versionName, {
-        projectId: arg.projectId,
+      context.log('creating-deployment', (message) => message.tag('progress').append('Creating deployment...'))
+
+      const deployment = await cloudApi.createDeployment({
+        apiKey: arg.apiKey,
+        versionName: arg.versionName,
         environmentId: arg.environmentId,
-        envFile: arg.envFile,
       })
+
+      context.log('deployment-created', (message) => message.tag('success').append('Deployment created'))
+      context.log('uploading-artifacts', (message) => message.tag('progress').append('Uploading artifacts...'))
+
+      await uploadArtifacts(builder, deployment.deploymentToken, listener)
+
+      context.log('starting-deployment', (message) => message.tag('progress').append('Starting deployment...'))
+
+      await deploy({
+        envVars: loadEnvData(arg.envFile, context),
+        deploymentToken: deployment.deploymentToken,
+        builder,
+      })
+
       context.exit(0)
     }),
   )

ObiyaDev/obiya

packages/snap/src/cloud/cli/deploy.ts
@@ -1,61 +1,58 @@
 import colors from 'colors'
-import path from 'path'
-import { build } from '../build'
+import { buildValidation } from '../build/build-validation'
 import { cloudCli } from '../cli'
 import { handler } from '../config-utils'
-import { VersionManager } from '../deploy/deploy'
-import { validateStepsConfig } from '../deploy/utils/validation'
+import { build } from '../new-deployment/build'
+import { cloudApi } from '../new-deployment/cloud-api'
+import { deploy } from '../new-deployment/deploy'
+import { CliListener } from '../new-deployment/listeners/cli-listener'
+import { uploadArtifacts } from '../new-deployment/upload-artifacts'
+import { loadEnvData } from '../new-deployment/utils/load-env-data'
 
 cloudCli
   .command('deploy')
   .description('Deploy a new version to Motia Cloud')
   .requiredOption('-k, --api-key <key>', 'The API key for authentication', process.env.MOTIA_API_KEY)
   .requiredOption('-v, --version-name <version>', 'The version to deploy')
-  .option('-p, --project-id <id>', 'Override the selected project')
-  .option('-s, --environment-id <id>', 'Override the selected environment')
+  .option('-p, --project-id <id>', 'Project ID')
+  .option('-s, --environment-id <id>', 'Environment ID')
   .option('-e, --env-file <path>', 'Path to environment file')
   .action(
     handler(async (arg, context) => {
-      const versionManager = new VersionManager()
-      const builder = await build(context)
-      const { errors, warnings } = validateStepsConfig(builder)
+      const listener = new CliListener(context)
+      const builder = await build(listener)
+      const isValid = buildValidation(builder, listener)
 
-      if (warnings.length > 0) {
-        warnings.map((warning, index) => {
-          context.log(`build-warnings-${index}`, (message) => {
-            message.tag('warning').append(warning.message)
-          })
-        })
-      }
-
-      if (errors.length > 0) {
-        context.log('build-failed', (message) => {
-          message.box(['Unable to deploy to Motia Cloud, please fix the following errors'], 'red')
-        })
-        console.log('')
-
-        const errorTag = colors.red('✗ [ERROR]')
-
-        errors.map((error) => {
-          const relativePath = path.relative(builder.projectDir, error.step.filePath)
-          const filePath = colors.gray(`[${relativePath}]`)
-          console.log(`${errorTag} ${filePath} ${error.message}`)
-        })
-
-        console.log(colors.gray('\n--------------------------------\n'))
-        context.log('build-failed-end', (message) => {
-          message.tag('failed').append('Deployment canceled', 'red')
-        })
+      if (!isValid) {
         process.exit(1)
       }
 
-      console.log(colors.green('✓ [SUCCESS]'), 'Build completed')
+      context.log('build-completed', (message) => message.tag('success').append('Build completed'))
+      context.log('creating-deployment', (message) => message.tag('progress').append('Creating deployment...'))
 
-      await versionManager.deploy(context, process.cwd(), arg.versionName, {
-        projectId: arg.projectId,
+      const deployment = await cloudApi.createDeployment({
+        apiKey: arg.apiKey,
+        versionName: arg.versionName,
         environmentId: arg.environmentId,
-        envFile: arg.envFile,
       })
+
+      context.log('creating-deployment', (message) => message.tag('success').append('Deployment created'))
+      context.log('uploading-artifacts', (message) => message.tag('progress').append('Uploading artifacts...'))
+
+      await uploadArtifacts(builder, deployment.deploymentToken, listener)
+
+      context.log('uploading-artifacts', (message) => message.tag('success').append('Artifacts uploaded'))
+      context.log('starting-deployment', (message) => message.tag('progress').append('Starting deployment...'))
+
+      await deploy({
+        envVars: loadEnvData(arg.envFile, context),
+        deploymentId: deployment.deploymentId,
+        deploymentToken: deployment.deploymentToken,
+        builder,
+        listener,
+        context,
+      })
+
       context.exit(0)
     }),
   )